376b27b723d45e7d0c511c487363c8d0cb731dc8
Paritosh Shah <shah.paritosh@gmail.com>
2011-02-19 04:08:55 -0500

    Initial import

---
+++ src/com/limebits/paritosh/sportsalarm/SportsAlarm.java
@@ -0,0 +1,13 @@
+package com.limebits.paritosh.sportsalarm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SportsAlarm extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
