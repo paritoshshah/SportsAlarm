6d10a13fa3da6d6e8f94efaf1377734caf24e464
Paritosh Shah <shah.paritosh@gmail.com>
2011-02-21 19:35:57 -0500

    cleaned up the "repeating alarm"  we don't need.

--- src/com/limebits/paritosh/sportsalarm/SportsAlarm.java
+++ src/com/limebits/paritosh/sportsalarm/SportsAlarm.java
@@ -20,7 +20,6 @@ import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.os.SystemClock;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -41,10 +40,6 @@ public class SportsAlarm extends Activity {
         // Watch for button clicks.
         Button button = (Button)findViewById(R.id.one_shot);
         button.setOnClickListener(mOneShotListener);
-        button = (Button)findViewById(R.id.start_repeating);
-        button.setOnClickListener(mStartRepeatingListener);
-        button = (Button)findViewById(R.id.stop_repeating);
-        button.setOnClickListener(mStopRepeatingListener);
     }
 
     private OnClickListener mOneShotListener = new OnClickListener() {
@@ -72,60 +67,6 @@ public class SportsAlarm extends Activity {
                 mToast.cancel();
             }
             mToast = Toast.makeText(SportsAlarm.this, R.string.one_shot_scheduled,
-                    Toast.LENGTH_LONG);
-            mToast.show();
-        }
-    };
-
-    private OnClickListener mStartRepeatingListener = new OnClickListener() {
-        public void onClick(View v) {
-            // When the alarm goes off, we want to broadcast an Intent to our
-            // BroadcastReceiver.  Here we make an Intent with an explicit class
-            // name to have our own receiver (which has been published in
-            // AndroidManifest.xml) instantiated and called, and then create an
-            // IntentSender to have the intent executed as a broadcast.
-            // Note that unlike above, this IntentSender is configured to
-            // allow itself to be sent multiple times.
-            Intent intent = new Intent(SportsAlarm.this, RepeatingAlarm.class);
-            PendingIntent sender = PendingIntent.getBroadcast(SportsAlarm.this,
-                    0, intent, 0);
-
-            // We want the alarm to go off 30 seconds from now.
-            long firstTime = SystemClock.elapsedRealtime();
-            firstTime += 15*1000;
-
-            // Schedule the alarm!
-            AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
-            am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                            firstTime, 15*1000, sender);
-
-            // Tell the user about what we did.
-            if (mToast != null) {
-                mToast.cancel();
-            }
-            mToast = Toast.makeText(SportsAlarm.this, R.string.repeating_scheduled,
-                    Toast.LENGTH_LONG);
-            mToast.show();
-        }
-    };
-
-    private OnClickListener mStopRepeatingListener = new OnClickListener() {
-        public void onClick(View v) {
-            // Create the same intent, and thus a matching IntentSender, for
-            // the one that was scheduled.
-            Intent intent = new Intent(SportsAlarm.this, RepeatingAlarm.class);
-            PendingIntent sender = PendingIntent.getBroadcast(SportsAlarm.this,
-                    0, intent, 0);
-
-            // And cancel the alarm.
-            AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
-            am.cancel(sender);
-
-            // Tell the user about what we did.
-            if (mToast != null) {
-                mToast.cancel();
-            }
-            mToast = Toast.makeText(SportsAlarm.this, R.string.repeating_unscheduled,
                     Toast.LENGTH_LONG);
             mToast.show();
         }
